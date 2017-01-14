595409221d8a388f00a5c1bafa052564463bc5f6
eidy <eidy@practicalcybernetics.com>
2017-01-14 11:30:53 +1000

    Remove confidentials

--- tools/trimdropbox.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DROPBOXLOC=/home/ubuntu/Dropbox/AndroidBuilds
-cd $DROPBOXLOC
-ls -tr eidy-??-debug???.apk | head -n -2 | xargs --no-run-if-empty rm
-ls -tr eidy-??-debug??.apk | head -n -2 | xargs --no-run-if-empty rm
